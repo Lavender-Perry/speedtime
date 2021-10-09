cb94cebf1400220cc888f03439b1db55bf6e1a72
Lavender Perry <endie2@protonmail.com>
2021-10-09 10:57:17 -0700

    Make fgets_no_newline code less stupid

--- src/utils.c
+++ src/utils.c
@@ -3,9 +3,9 @@
 
 char* fgets_no_newline(char* restrict buf, size_t buflen, FILE* restrict fp) {
     if (fgets(buf, buflen, fp) != NULL) {
-        const char* newline_loc = strchr(buf, '\n');
+        char* newline_loc = strchr(buf, '\n');
         if (newline_loc)
-            buf[newline_loc - buf] = '\0';
+            *newline_loc = '\0';
         return buf;
     }
     return NULL;
